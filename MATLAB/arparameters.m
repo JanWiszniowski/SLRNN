9803ccaba480f0579ec7951c1ab5dabecb448915
Jan Wiszniowski <60662015+JanWiszniowski@users.noreply.github.com>
2020-02-07 09:28:17 +0100

    Update arparameters.m

--- MATLAB/arparameters.m
+++ MATLAB/arparameters.m
@@ -1,4 +1,10 @@
 function [parameter] = arparameters(parametername,defaultvalue,varargin)
+% function [parameter] = arparameters(parametername,defaultvalue,parName1,parValue1,...)
+% Checks if the parametername exist in the list of function call pairs
+% of parameters ...,parName*, parValue*,... following the defaultvalue.
+% If exist the name and corresponding value, the function returns
+% the corresponding value. Otherword, it returns defaultvalue.
+
 parameter = defaultvalue ;
 N = length(varargin) ;
 for i = 1:length(varargin)
