ed0447b136dce867d347f1547185e4acf493a4d4
Jan Wiszniowski <60662015+JanWiszniowski@users.noreply.github.com>
2020-02-07 09:23:29 +0100

    Update arispar.m

--- MATLAB/arispar.m
+++ MATLAB/arispar.m
@@ -1,4 +1,8 @@
 function [parameter] = arispar(parametername,varargin)
+% function [parameter] = aroptions(parametername,...,parName1,...)
+% Checks if the parametername exist in the list of function call parameters following
+% the parametername. If exist the name and corresponding value,
+% the function returns true. Otherworld, it returns false.
 parameter = 0 ;
 N = length(varargin) ;
 for i = 1:length(varargin)-1
