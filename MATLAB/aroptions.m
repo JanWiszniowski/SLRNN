862e941331dfbb9d181053ed8d6e76ac88acfd49
Jan Wiszniowski <60662015+JanWiszniowski@users.noreply.github.com>
2020-02-07 09:21:57 +0100

    Update aroptions.m

--- MATLAB/aroptions.m
+++ MATLAB/aroptions.m
@@ -1,4 +1,8 @@
 function [parameter] = aroptions(parametername,varargin)
+% function [parameter] = aroptions(parametername,parName1, parValue1,...)
+% Checks if the parametername exist in the list of pairs ...,parName*, parName*,...
+% following the parametername. If exist the name and corresponding value,
+% the function returns true. Otherword, it returns false.
 parameter = false ;
 N = length(varargin) ;
 for i = 1:length(varargin)
